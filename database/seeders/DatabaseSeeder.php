5040c91ddf56f5a3bf7882ec49c818e04630da0d
mauro <mauro.emc@outlook.com>
2025-04-09 16:27:11 -0300

    Revert "Implemented seeder for contacts table"
    This reverts commit 3f48064869a1149b9d825fe9685de4a7ed50bd1e.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,8 +10,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(ContactSeeder $contactSeerder): void
+    public function run(): void
     {
-        $contactSeerder->run();
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
