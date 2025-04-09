a89a72a1e61e4f10631fd5ff66e607cce043353f
mauro <mauro.emc@outlook.com>
2025-04-09 19:52:14 -0300

    issue - Fix error flash message component

--- resources/views/components/flash-alerts.blade.php
+++ resources/views/components/flash-alerts.blade.php
@@ -4,7 +4,7 @@
         <div class="col-12">
             <div class="mb-3">
                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <i class="fa fa-info-circle" aria-hidden="true"></i> {{session('success-message')}}
+                <i class="fa fa-info-circle" aria-hidden="true"></i> {{session('error-message')}}
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
             </div>
